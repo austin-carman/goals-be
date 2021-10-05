d87437a481a8f845cc2e30f659a0dae03361b44f
Austin Carman <abcarman12@gmail.com>
2021-10-05 13:12:59 -1000

    goals table completed with PK, FK, other columns

--- api/data/migrations/20210124181032_first-migration.js
+++ api/data/migrations/20210124181032_first-migration.js
@@ -1,14 +1,26 @@
 exports.up = async (knex) => {
   await knex.schema
-    .createTable('users', (users) => {
-      users.increments('user_id')
-      users.string('first_name', 200).notNullable()
-      users.string('last_name', 200).notNullable()
-      users.string('username', 200).notNullable()
-      users.string('password', 200).notNullable()
+    .createTable("users", (users) => {
+      users.increments("user_id")
+      users.string("first_name", 200).notNullable()
+      users.string("last_name", 200).notNullable()
+      users.string("username", 200).notNullable()
+      users.string("password", 200).notNullable()
+    })
+    .createTable("goals", (goals) => {
+      goals.increments("goal_id")
+      goals.integer("user_id")
+        .references("user_id")
+        .inTable("users")
+        .notNullable()
+        .unsigned()
+        .onDelete("CASCADE")
+        .onUpdate("CASCADE")
+      goals.string("goal_title", 200).notNullable()
+      goals.boolean("goal_completed").defaultTo("false")
     })
 }
 
 exports.down = async (knex) => {
-  await knex.schema.dropTableIfExists('users')
+  await knex.schema.dropTableIfExists("users")
 }
