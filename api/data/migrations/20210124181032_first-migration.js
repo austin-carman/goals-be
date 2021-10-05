996781fae2a9a8bc1c1cc1403f92b395ac028819
Austin Carman <abcarman12@gmail.com>
2021-10-05 12:54:51 -1000

    users table

--- api/data/migrations/20210124181032_first-migration.js
+++ api/data/migrations/20210124181032_first-migration.js
@@ -2,9 +2,10 @@ exports.up = async (knex) => {
   await knex.schema
     .createTable('users', (users) => {
       users.increments('user_id')
+      users.string('first_name', 200).notNullable()
+      users.string('last_name', 200).notNullable()
       users.string('username', 200).notNullable()
       users.string('password', 200).notNullable()
-      users.timestamps(false, true)
     })
 }
 
