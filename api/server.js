60453e6b13b816ffa4068b8f5c6848d25338564f
Austin Carman <abcarman12@gmail.com>
2021-10-06 09:32:31 -1000

    commented out unnecessary code from scaffold

--- api/server.js
+++ api/server.js
@@ -1,29 +1,37 @@
 const express = require('express')
 const helmet = require('helmet')
 const cors = require('cors')
-const db = require('./data/db-config')
+// const db = require('./data/db-config') // don't need this in server
 
-function getAllUsers() { return db('users') }
+// function getAllUsers() { return db('users') } // don't need this in server
 
-async function insertUser(user) {
-  // WITH POSTGRES WE CAN PASS A "RETURNING ARRAY" AS 2ND ARGUMENT TO knex.insert/update
-  // AND OBTAIN WHATEVER COLUMNS WE NEED FROM THE NEWLY CREATED/UPDATED RECORD
-  // UNLIKE SQLITE WHICH FORCES US DO DO A 2ND DB CALL
-  const [newUserObject] = await db('users').insert(user, ['user_id', 'username', 'password'])
-  return newUserObject // { user_id: 7, username: 'foo', password: 'xxxxxxx' }
-}
+// don't need this in server
+// async function insertUser(user) {
+//   // WITH POSTGRES WE CAN PASS A "RETURNING ARRAY" AS 2ND ARGUMENT TO knex.insert/update
+//   // AND OBTAIN WHATEVER COLUMNS WE NEED FROM THE NEWLY CREATED/UPDATED RECORD
+//   // UNLIKE SQLITE WHICH FORCES US DO DO A 2ND DB CALL
+//   const [newUserObject] = await db('users').insert(user, ['user_id', 'username', 'password'])
+//   return newUserObject // { user_id: 7, username: 'foo', password: 'xxxxxxx' }
+// }
+
+// Routers go here
 
 const server = express()
 server.use(express.json())
 server.use(helmet())
 server.use(cors())
 
-server.get('/api/users', async (req, res) => {
-  res.json(await getAllUsers())
-})
+// Route base URL here
 
-server.post('/api/users', async (req, res) => {
-  res.status(201).json(await insertUser(req.body))
-})
+
+// don't need in server
+// server.get('/api/users', async (req, res) => {
+//   res.json(await getAllUsers())
+// })
+
+// don't need in server
+// server.post('/api/users', async (req, res) => {
+//   res.status(201).json(await insertUser(req.body))
+// })
 
 module.exports = server
