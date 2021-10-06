39ba94668aa5a77419cf4e7524bcffbd56dcda27
Austin Carman <abcarman12@gmail.com>
2021-10-06 10:30:04 -1000

    user model  connected to router. Need to flesh out

---
+++ api/models/user-model.js
@@ -0,0 +1,9 @@
+const db = require("../../data/db-config");
+
+async function findUserBy(filter) {
+    console.log("model is connected");
+};
+
+module.exports = {
+    findUserBy
+}
--- api/routers/user-router.js
+++ api/routers/user-router.js
@@ -1,7 +1,11 @@
 const router = require("express").Router();
+const User = require("../models/user-model");
 
 router.get("/", (req, res, next) => {
-    res.json({message: "user router is wired"})
+    const filter = { user_id: 1 };
+    User.findUserBy(filter)
+        .then(x => res.json({message: "working!!!"}))
+        .catch(err => console.log("error: ", err));
 })
 
 module.exports = router
