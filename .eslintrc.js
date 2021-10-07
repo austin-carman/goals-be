e6fd0b44846ff83e3d4a684eb9338411f3cf2952
Austin Carman <abcarman12@gmail.com>
2021-10-06 14:50:06 -1000

    linter changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,16 @@
 module.exports = {
-    env: {
-      node: true,
-      commonjs: true,
-      es2021: true,
-      jest: true,
-    },
-    extends: ['eslint:recommended', 'plugin:prettier/recommended'],
-    parserOptions: {
-      ecmaVersion: 12,
-    },
-    plugins: ['prettier'],
-    rules: {
-        "indent": ["error", 4],
-        "quotes": ["error", "double"],
-        "semi": ["error", "always"],
-    },
-    overrides: [
-      {
-        files: ['__tests__/*', '__tests__/**/*'],
-        env: {
-          jest: true,
-        },
-      },
-    ],
-  };
-  
+  "env": {
+    "commonjs": true,
+    "es2021": true,
+    "node": true
+  },
+  "extends": "eslint:recommended",
+  "parserOptions": {
+    "ecmaVersion": 12
+  },
+  "rules": {
+    "indent": ["error", 2],
+    "quotes": ["error", "double"],
+    "semi": ["error", "always"],
+  }
+};
