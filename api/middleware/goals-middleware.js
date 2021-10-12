d0d14016349ad5690cacb67e9c69a867ba239312
Austin Carman <abcarman12@gmail.com>
2021-10-12 10:20:48 -1000

    comments and structure for needed middleware

---
+++ api/middleware/goals-middleware.js
@@ -0,0 +1,34 @@
+
+
+const restricted = (req, res, next) => {
+
+};
+
+const validateUserId =  (req, res, next) => {
+
+};
+
+const validateGoalId =  (req, res, next) => {
+
+};
+
+const validateStepId =  (req, res, next) => {
+
+};
+
+const validateNewGoal = (req, res, next) => {
+
+};
+
+const validateEditGoal = (req, res, next) => {
+
+};
+
+module.exports = {
+  restricted,
+  validateUserId,
+  validateGoalId,
+  validateStepId,
+  validateNewGoal,
+  validateEditGoal,
+};
--- api/routers/goals-router.js
+++ api/routers/goals-router.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const Goals = require("../models/goals-model");
 
 // Get all goals for specified user
-router.get("/:user_id", (req, res, next) => {
+router.get("/:user_id", (req, res, next) => { // restricted, validate user_id
   Goals.getUserGoals(1)
     .then(goals => {
       res.json(goals);
@@ -11,7 +11,7 @@ router.get("/:user_id", (req, res, next) => {
 });
 
 // Create a new goal for specified user
-router.post("/new-goal/:user_id", (req, res, next) => {
+router.post("/new-goal/:user_id", (req, res, next) => { // restricted, validate user_id, validate body
   Goals.newGoal(req.params.user_id, req.body)
     .then(goal => {
       res.json(goal);
@@ -20,7 +20,7 @@ router.post("/new-goal/:user_id", (req, res, next) => {
 });
 
 // Edit existing specified goal
-router.put("/edit/:goal_id", (req, res, next) => {
+router.put("/edit/:goal_id", (req, res, next) => { // restricted, validate goal_id, validate body
   Goals.editGoal(req.params.goal_id, req.body)
     .then(goal => {
       res.json(goal);
@@ -29,7 +29,7 @@ router.put("/edit/:goal_id", (req, res, next) => {
 });
 
 // Delete specified goal and all associated steps
-router.delete("/delete-goal/:goal_id", (req, res, next) => {
+router.delete("/delete-goal/:goal_id", (req, res, next) => { // restricted, validate goal_id
   Goals.deleteGoal(req.params.goal_id)
     .then(deleted => {
       res.status(200).json(deleted);
@@ -38,7 +38,7 @@ router.delete("/delete-goal/:goal_id", (req, res, next) => {
 });
 
 // Delete specified step
-router.delete("/delete-step/:step_id", (req, res, next) => {
+router.delete("/delete-step/:step_id", (req, res, next) => { // restricted, validate step_id
   Goals.deleteStep(req.params.step_id)
     .then(deleted => {
       res.status(200).json(deleted);
